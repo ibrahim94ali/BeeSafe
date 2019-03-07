cd771a2e05e55147e4fd3f5fc02d2214d32fbb89
Ibrahim Aliu <aliui@itu.edu.tr>
2019-03-07 19:04:39 +0300

    Fingerprint scanner is added

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
   styleUrls: ['tabs.page.scss']
 })
-export class TabsPage {}
+export class TabsPage {
+  constructor(private faio: FingerprintAIO, private platform: Platform) { }
+  ngOnInit()
+  {
+    this.faio.show({
+      clientId: 'Fingerprint-Demo',
+      clientSecret: 'password', //Only necessary for Android
+      disableBackup:false,  //Only for Android(optional)
+      localizedFallbackTitle: 'Use Pin', //Only for iOS
+      localizedReason: 'Please authenticate' //Only for iOS
+  })
+  .then((result: any) => console.log(result))
+  .catch((error: any) => {console.log(error); 
+    navigator['app'].exitApp();
+  });
+
+  }
+}
